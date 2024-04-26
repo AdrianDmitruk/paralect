bf6673605f96e955b8f9520bfccd23a6973ec60b
Adrian <dmitrukadrian@gmail.com>
2024-04-26 04:06:33 +0300

    feat: add test api

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/test",
+        destination:
+          "https://api.themoviedb.org/3/discover/movie?api_key=7b9e692ff9495f98fbb3875034e3a8f2",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
 export default function Test() {
-  return <>asdasd</>;
+  const [data, setData] = useState<any>([]);
+
+  useEffect(() => {
+    fetch("/api/test")
+      .then((response) => response.json())
+      .then((response) => setData(response))
+      .catch((err) => console.error(err));
+  }, []);
+
+  console.log(data);
+
+  return (
+    <>
+      {data?.results?.map((item: any) => (
+        <div key={item.id}>
+          <p>{item.title}</p>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${item.poster_path}`}
+            alt="asdsad"
+          />
+        </div>
+      ))}
+    </>
+  );
 }
